0b9da618e860a745a52dfd843994a54666de35ea
Tee Parham <tee@neighborland.com>
2018-04-05 21:41:05 -0600

    Do not generate sass map files

--- watchit.sh
+++ watchit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sass --watch stylesheets/styles.scss:stylesheets/styles.css &
+sass --watch stylesheets/styles.scss:stylesheets/styles.css --sourcemap=none &
 watch haml index.haml index.html
